e8c2e0aa14698dbd6192975800877e2b2be043f3
jaswsinc <jaswsinc@wsharks.com>
2016-05-27 19:12:24 -0800

    Phing release of v160528.11278.

---
+++ src/includes/classes/Utils/Foo.php
@@ -0,0 +1,32 @@
+<?php
+declare (strict_types = 1);
+namespace WebSharks\WpSharks\Skeleton\Classes\Utils;
+
+use WebSharks\WpSharks\Skeleton\Classes;
+use WebSharks\WpSharks\Skeleton\Interfaces;
+use WebSharks\WpSharks\Skeleton\Traits;
+#
+use WebSharks\WpSharks\Skeleton\Classes\AppFacades as a;
+use WebSharks\WpSharks\Skeleton\Classes\SCoreFacades as s;
+use WebSharks\WpSharks\Skeleton\Classes\CoreFacades as c;
+#
+use WebSharks\WpSharks\Core\Classes as SCoreClasses;
+use WebSharks\WpSharks\Core\Interfaces as SCoreInterfaces;
+use WebSharks\WpSharks\Core\Traits as SCoreTraits;
+#
+use WebSharks\Core\WpSharksCore\Classes as CoreClasses;
+use WebSharks\Core\WpSharksCore\Classes\Core\Base\Exception;
+use WebSharks\Core\WpSharksCore\Interfaces as CoreInterfaces;
+use WebSharks\Core\WpSharksCore\Traits as CoreTraits;
+#
+use function assert as debug;
+use function get_defined_vars as vars;
+
+/**
+ * Foo.
+ *
+ * @since 16xxxx Initial release.
+ */
+class Foo extends SCoreClasses\SCore\Base\Core
+{
+}
---
+++ src/includes/wp-sharks-core-rv.php
@@ -0,0 +1,11 @@
+<?php
+declare (strict_types = 1);
+namespace WebSharks\WpSharks\Skeleton;
+
+if (!defined('WPINC')) {
+    exit('Do NOT access this file directly: '.basename(__FILE__));
+}
+$GLOBALS['wp_sharks_core_rv'] = [
+    'min' => '160528.1258', //wp-sharks-core-required-version//
+    'max' => '', //wp-sharks-core-max-compatible-version//
+];
